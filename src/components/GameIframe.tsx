8be68d0ffe473f2d9d0cf00c7cc4551f38d99c7d
Mr. loop <scorp742@gmail.com>
2026-03-01 10:20:28 +0200

    Update GameIframe.tsx

--- src/components/GameIframe.tsx
+++ src/components/GameIframe.tsx
@@ -2,7 +2,7 @@ const GameIframe = () => {
   return (
     <div className="w-full h-[800px] mt-4 rounded-lg overflow-hidden border border-foreground/5 shadow-md">
       <iframe
-        src="https://go.aff.oddsbest.co/uvvguo18"
+        src="https://go.aff.oddsbest.co/kbrgjgg0"
         className="w-full h-full border-none"
         title="Lotto24"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen; payment; camera; microphone"
